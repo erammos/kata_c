632f9a17b8fd566e42331a1db94ccb16113e203d
Coursera Learner <nobody@nowhere.nul>
2020-12-28 19:32:54 +0000

    reverse

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -4,6 +4,14 @@
 
 void reverse(char * str) {
   //WRITE ME!
+size_t n = strlen(str);
+
+for(int i = 0; i < n / 2; i++)
+{
+ int temp = str[i];
+ str[i]= str[n - i - 1];
+ str[n - i -1] = temp;
+}
 }
 
 int main(void) {
